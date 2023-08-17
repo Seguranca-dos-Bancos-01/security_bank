8d3cfb8e2e22798c8e66d1489d3fbd412e99d133
Guigon14 <guilherme.gsantos@sptech.school>
2023-08-17 20:25:59 -0300

    Script de automação captura de dados

--- Documentos/script_Python.py
+++ Documentos/script_Python.py
@@ -2,19 +2,22 @@ import time
 import psutil
 
 def monitorar_recursos(intervalo):
-    while True:
+    while (True):
         uso_cpu = psutil.cpu_percent(interval=0.5)
         uso_disco = psutil.disk_usage('/').percent
         uso_memoria = psutil.virtual_memory().percent
-        instabilidade_rede = psutil.net_io_counters.percent
-
+        rede = psutil.net_io_counters()
+        entrada = rede[4]
+        saida = rede[5]
+        
         print(f"Uso da CPU: {uso_cpu}%")
         print(f"Uso do Disco: {uso_disco}%")
         print(f"Uso da Memória: {uso_memoria}%")
-        print("=" * 40)
+        print(f'Errin:{entrada}\nErrout:{saida}')
+        print("=" * 40)     
 
         time.sleep(intervalo)
 
-intervalo_de_atualizacao = 5  # Intervalo de atualização em segundos
+intervalo_de_atualizacao = 2  
 monitorar_recursos(intervalo_de_atualizacao)
 
