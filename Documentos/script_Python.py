6f9fdf31580d09e91c067d7abc0a887e610d1411
Chris <christian.bellei@sptech.school>
2023-08-28 13:45:29 -0300

    Mexendo

--- Documentos/script_Python.py
+++ Documentos/script_Python.py
@@ -3,17 +3,19 @@ import psutil
 
 def monitorar_recursos(intervalo):
     while (True):
-        uso_cpu = psutil.cpu_percent(interval=0.5)
-        uso_disco = psutil.disk_usage('/').percent
-        uso_memoria = psutil.virtual_memory().percent
-        rede = psutil.net_io_counters()
-        entrada = rede[4]
-        saida = rede[5]
         
-        print(f"Uso da CPU: {uso_cpu}%")
-        print(f"Uso do Disco: {uso_disco}%")
-        print(f"Uso da Memória: {uso_memoria}%")
-        print(f'Errin:{entrada}\nErrout:{saida}')
+        uso_disco1 = psutil.disk_usage('/').percent
+        uso_disco2 = psutil.disk_usage('/').percent
+        uso_disco3 = psutil.disk_usage('/').percent
+
+#Monitoramento do DISCO2
+        
+        uso_disco2 = uso_disco1 * 0.95        
+        uso_disco3 = uso_disco2 * 3
+        print(f"Uso do Disco: {round(uso_disco1,1)}%")
+        print(f"Uso do Disco: {round(uso_disco2,1)}%")
+        print(f"Uso do Disco: {round(uso_disco3,1)}%")
+        
         print("=" * 40)     
 
         time.sleep(intervalo)
@@ -21,3 +23,6 @@ def monitorar_recursos(intervalo):
 intervalo_de_atualizacao = 2  
 monitorar_recursos(intervalo_de_atualizacao)
 
+
+meu_so = platform.system()
+print("SO que eu uso : ",meu_so)
