62654020cc29961b725136297f42acbcd66b88f7
russo-natalia <natalia.russo@sptech.school>
2023-10-23 20:19:45 -0300

    alterando css do gerenciar acesso

--- SecurityBank/site/src/database/config.js
+++ SecurityBank/site/src/database/config.js
@@ -22,7 +22,7 @@ var mySqlConfig = {
     host: "localhost",
     database: "SecurityBank",
     user: "root",
-    password: "justin123",
+    password: "Juronaty@23",
 };
 
 function executar(instrucao) {
