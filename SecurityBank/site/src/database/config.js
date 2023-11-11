9e381674b1a513a1722ae0e7239907b7491b2b37
Natália Campioli Russo <natalia.russo@sptech.school>
2023-11-11 14:50:03 -0300

    add css na página de servidor individual

--- SecurityBank/site/src/database/config.js
+++ SecurityBank/site/src/database/config.js
@@ -22,7 +22,7 @@ var mySqlConfig = {
     host: "localhost",
     database: "SecurityBank",
     user: "root",
-    password: "Pedroca12@",
+    password: "Juronaty@23",
 };
 
 function executar(instrucao) {
