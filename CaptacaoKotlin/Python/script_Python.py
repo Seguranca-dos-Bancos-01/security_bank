22f418189901fd1802f7a8a539093025ad2cb4ee
Be-Oliveira <130523077+Be-Oliveira@users.noreply.github.com>
2023-11-13 13:52:00 -0300

    fix: python

--- CaptacaoKotlin/Python/script_Python.py
+++ CaptacaoKotlin/Python/script_Python.py
@@ -1,9 +1,12 @@
 import time
 import psutil
+import requests
+import json 
+
 
 def monitorar_recursos(intervalo):
     while (True):
-<<<<<<< HEAD
+# <<<<<<< HEAD
         
         uso_disco1 = psutil.disk_usage('/').percent
         uso_disco2 = psutil.disk_usage('/').percent
@@ -17,7 +20,7 @@ def monitorar_recursos(intervalo):
         print(f"Uso do Disco: {round(uso_disco2,1)}%")
         print(f"Uso do Disco: {round(uso_disco3,1)}%")
         
-=======
+# =======
         uso_cpu = psutil.cpu_percent(interval=0.5)
         uso_disco = psutil.disk_usage('/').percent
         uso_memoria = psutil.virtual_memory().percent
@@ -35,10 +38,82 @@ def monitorar_recursos(intervalo):
         print(f"Entrada de Bytes: {entradaBytes}\nSaída de Bytes: {saidaBytes}")
         print(f"Entrada de Pacotes: {entradaPacotes}\nSaída de Pacotes: {saidaPacotes}")
         print(f'Errin:{errin}\nErrout:{errout}')
->>>>>>> 291e8583fdfede5acb16e6d7cf5a4d10d4c5f4b4
         print("=" * 40)     
 
         time.sleep(intervalo)
+
+        webhook = "https://hooks.slack.com/services/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"
+
+
+        # atenção 
+        if uso_cpu  >= 18 and uso_cpu < 23 :
+            
+            alerta = {"text": f"Atenção! CPU com {uso_cpu}% de uso"}
+            
+
+            requests.post(webhook, data=json.dumps(alerta))
+            
+        if uso_disco >= 50 and uso_disco < 70 :
+           
+            alerta = {"text": f"Atenção! Disco com {uso_disco}% de uso "}
+           
+
+            requests.post(webhook, data=json.dumps(alerta))
+
+        if uso_memoria >= 45 and uso_memoria < 55:
+           
+            alerta = {"text": f"Atenção! Memória RAM com {uso_memoria}% de uso"}
+          
+
+            requests.post(webhook, data=json.dumps(alerta))
+
+
+            #emergência
+        if uso_cpu  >= 23 and uso_cpu < 50 :
+            
+            alerta = {"text": f"Emergência! CPU com {uso_cpu}% de uso"}
+            
+
+            requests.post(webhook, data=json.dumps(alerta))
+            
+        if uso_disco >= 70 and uso_disco < 80 :
+           
+            alerta = {"text": f"Emergência! Disco com {uso_disco}% de uso "}
+           
+
+            requests.post(webhook, data=json.dumps(alerta))
+
+        if uso_memoria >= 55 and uso_memoria < 65:
+           
+            alerta = {"text": f"Emergência! Memória RAM com {uso_memoria}% de uso"}
+          
+
+            requests.post(webhook, data=json.dumps(alerta))
+
+
+            #urgência
+        if uso_cpu  >= 50:
+            
+            alerta = {"text": f"Urgência! CPU com {uso_cpu}% de uso"}
+            
+
+            requests.post(webhook, data=json.dumps(alerta))
+            
+        if uso_disco >= 80 :
+           
+            alerta = {"text": f"Urgência! Disco com {uso_disco}% de uso "}
+           
+
+            requests.post(webhook, data=json.dumps(alerta))
+
+        if uso_memoria >= 65:
+           
+            alerta = {"text": f"Urgência! Memória RAM com {uso_memoria}% de uso"}
+          
+
+            requests.post(webhook, data=json.dumps(alerta))
+
+
 
 intervalo_de_atualizacao = 2  
 monitorar_recursos(intervalo_de_atualizacao)
