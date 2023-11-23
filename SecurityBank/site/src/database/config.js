202843daeaf30d2c41d24afde79fcb6fcf439e41
russo-natalia <natalia.russo@sptech.school>
2023-11-23 11:36:11 -0300

    Adicionando a troca de abas

--- SecurityBank/site/src/database/config.js
+++ SecurityBank/site/src/database/config.js
@@ -21,8 +21,8 @@ var sqlServerConfig = {
 var mySqlConfig = {
     host: "localhost",
     database: "SecurityBank",
-    user: "root",
-    password: "Juronaty@23",
+    user: "aluno",
+    password: "sptech",
 };
 
 function executar(instrucao) {
