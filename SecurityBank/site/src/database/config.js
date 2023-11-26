6009c293eb6600ff88e03029e767307aa60fe707
Gustavo Henrique Marchi de Oliveira <gustavo.holiveira@sptech.school>
2023-11-26 15:24:55 -0300

    alteração do banco para minhas configurações

--- SecurityBank/site/src/database/config.js
+++ SecurityBank/site/src/database/config.js
@@ -23,7 +23,7 @@ var mySqlConfig = {
     host: "localhost",
     database: "SecurityBank",
     user: "root",
-    password: "Juronaty@23",
+    password: "rootpassword",
 };
 
 function executar(instrucao) {
