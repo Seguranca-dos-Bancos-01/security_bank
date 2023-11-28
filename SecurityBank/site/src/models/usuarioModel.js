36f35edc630f70559a50d3ed255f627da4ffeaad
GustavoHMarchi <gustavo.holiveira@spetch.school>
2023-11-28 17:02:44 +0000

    selects usuarioModel alterados

--- SecurityBank/site/src/models/usuarioModel.js
+++ SecurityBank/site/src/models/usuarioModel.js
@@ -1,15 +1,19 @@
  database = require("../database/config")
 
 function autenticar(email, senha) {
-    // console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
-    var instrucao = `
-   
- select* from funcionarios WHERE 
- email = '${email}' AND 
- senha = '${senha}';
-    `;
-    console.log("Executando a instrução SQL: \n" + instrucao);
-    return database.executar(instrucao);
+    instrucaoSql = ``
+    if(process.env.AMBIENTE_PROCESSO == "producao"){
+        instrucaoSql `select* from funcionarios WHERE 
+        email = '${email}' AND 
+        senha = '${senha}';`
+    }else if(process.env.AMBIENTE_PROCESSO == "desenvolvimento"){
+        instrucaoSql `select* from funcionarios WHERE 
+        email = '${email}' AND 
+        senha = '${senha}';`
+    }
+
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql)
 }
 
 
@@ -17,83 +21,82 @@ function autenticar(email, senha) {
 
 
 function AtualizarSessionUsuario(id) {
-    // console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
-    var instrucao = `
-   
- select* from funcionarios WHERE 
- idFuncionarios = ${id}
-    `;
-    console.log("Executando a instrução SQL: \n" + instrucao);
-    return database.executar(instrucao);
+    instrucaoSql = ``
+
+    if(process.env.AMBIENTE_PROCESSO == "producao"){
+        instrucaoSql ` select* from funcionarios WHERE 
+        idFuncionarios = ${id};`
+    }else if(process.env.AMBIENTE_PROCESSO == "desenvolvimento"){
+        instrucaoSql ` select* from funcionarios WHERE 
+        idFuncionarios = ${id};`
+    }
+
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql)
 }
 
 
 function AtualizarSessionBanco(id) {
-    // console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
-    var instrucao = `
-   
- select* from banco WHERE 
- idBanco = ${id}
-    `;
-    console.log("Executando a instrução SQL: \n" + instrucao);
-    return database.executar(instrucao);
+    instrucaoSql = ``
+
+    if(process.env.AMBIENTE_PROCESSO == "producao"){
+        instrucaoSql `select* from banco WHERE 
+        idBanco = ${id};`
+    }else if(process.env.AMBIENTE_PROCESSO == "desenvolvimento"){
+        instrucaoSql `select* from banco WHERE 
+        idBanco = ${id};`
+    }
+
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql)
 }
 
-
-
-
-
-
-
-
-
-
-// Coloque os mesmos parâmetros aqui. Vá para a var instrucao
 function cadastrar(nome, email, cpf, telefone, senha, empresaId,) {
-    // console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha);
+    instrucaoSql = ``
 
-    // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
-    //  e na ordem de inserção dos dados.
-    var instrucao = `
-    INSERT INTO funcionarios (nome, email, cpf, telefone, senha, fkBanco, fkEscalonamento) VALUES ('${nome}', '${email}', '${cpf}', '${telefone}', '${senha}', ${empresaId}, 1);
-`;
+    if(process.env.AMBIENTE_PROCESSO == "producao"){
+        instrucaoSql `INSERT INTO funcionarios (nome, email, cpf, telefone, senha, fkBanco, fkEscalonamento) VALUES ('${nome}', '${email}', '${cpf}', '${telefone}', '${senha}', ${empresaId}, 3);`
+    }else if(process.env.AMBIENTE_PROCESSO == "desenvolvimento"){
+        instrucaoSql `INSERT INTO funcionarios (nome, email, cpf, telefone, senha, fkBanco, fkEscalonamento) VALUES ('${nome}', '${email}', '${cpf}', '${telefone}', '${senha}', ${empresaId}, 3);`
+    }
 
-    console.log("Executando a instrução SQL: \n" + instrucao);
-    return database.executar(instrucao);
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql)
 }
 
 function cadastrarServidor(apelidoServidor, soServidor, cpfRespServidor, ipServidor, fkBanco, fkPlano) {
-    // console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha);
+    instrucaoSql = ``
 
-    // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
-    //  e na ordem de inserção dos dados.
+    if(process.env.AMBIENTE_PROCESSO == "producao"){
+        instrucaoSql `INSERT INTO servidor (apelido, sistemaOperacional, cpfResponsavelLegal, enderecoIP, fkbanco, fkstatus, fkEspecificacoes, fkPlano) VALUES 
+        ('${apelidoServidor}','${soServidor}','${cpfRespServidor}','${ipServidor}',${fkBanco},1 ,1 ,${fkPlano})`
+    }else if(process.env.AMBIENTE_PROCESSO == "desenvolvimento"){
+        instrucaoSql `INSERT INTO servidor (apelido, sistemaOperacional, cpfResponsavelLegal, enderecoIP, fkbanco, fkstatus, fkEspecificacoes, fkPlano) VALUES 
+        ('${apelidoServidor}','${soServidor}','${cpfRespServidor}','${ipServidor}',${fkBanco},1 ,1 ,${fkPlano})`
+    }
 
-    //CONVERSAR COM GRUPO ()
-    var instrucao = `INSERT INTO servidor (apelido, sistemaOperacional, cpfResponsavelLegal, enderecoIP, fkbAnco, fkstatus, fkEspecificacoes, fkPlano) VALUES 
-            ('${apelidoServidor}','${soServidor}','${cpfRespServidor}','${ipServidor}',${fkBanco},1 ,1 ,${fkPlano})   
-`;
-
-    console.log("Executando a instrução SQL: \n" + instrucao);
-    return database.executar(instrucao);
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql)
 }
 
 
 function PuxarFkServidor(idUsuario, apelido) {
 
-    instrucaoSql = ''
+    instrucaoSql = ``
 
     if (process.env.AMBIENTE_PROCESSO == "producao") {
-        instrucaoSql = `SELECT idServidor AS fkP
+        instrucaoSql = `SELECT idServidor AS fkP,
+        apelido as apelido
         FROM Servidor
         WHERE apelido = '${apelido}' AND fkbanco = ${idUsuario};
     
     `;
     } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
         instrucaoSql = `
-        SELECT idServidor AS fkP,
-        apelido as apelido
-        FROM Servidor
-        WHERE apelido = '${apelido}' AND fkbanco = ${idUsuario};
+            SELECT idServidor AS fkP,
+            apelido as apelido
+            FROM Servidor
+            WHERE apelido = '${apelido}' AND fkbanco = ${idUsuario};
 
     `;
     } else {
@@ -106,87 +109,124 @@ function PuxarFkServidor(idUsuario, apelido) {
 }
 
 
+    function cadastrarServidorNuvem(dtCompra, dtVal, idUsuario) {
+        instrucaoSql = ``
 
+    if (process.env.AMBIENTE_PROCESSO == "producao") {
+        instrucaoSql = `INSERT INTO locacao (dataCompraLocacao, dataValidade, fkServidor,fkBanco,fkEspecificacoes,fkPlano) 
+        VALUES('${dtCompra}','${dtVal}',${idUsuario},1,1,1);
+    `;
+    } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
+        instrucaoSql = `
+        INSERT INTO locacao (dataCompraLocacao, dataValidade, fkServidor,fkBanco,fkEspecificacoes,fkPlano) 
+        VALUES('${dtCompra}','${dtVal}',${idUsuario},1,1,1);
+    `;
+    } else {
+        console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
+        return
+    }
 
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
 
-
-
-        function cadastrarServidorNuvem(dtCompra, dtVal, idUsuario) {
-
-
-    var instrucao = `INSERT INTO locacao (dataCompraLocacao, dataValidade, fkServidor) 
-    VALUES('${dtCompra}','${dtVal}',${idUsuario});
-      `;
-
-      console.log("Executando a instrução SQL: \n" + instrucao);
-      return database.executar(instrucao);
-    //     console.log("Executando a instrução SQL: \n" + instrucao3);
-    //     return database.executar(instrucao3);
     }
 
     function atualizarPerfil(nomePerfil, emailPerfil, cpfPerfil, telefonePerfil, senhaPerfil, idPerfil) {
-      
+        instrucaoSql = ``
 
-        // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
-        //  e na ordem de inserção dos dados.
-        var instrucao = `
-    update funcionarios set nome = '${nomePerfil}', email = '${emailPerfil}', senha = '${senhaPerfil}', telefone = '${telefonePerfil}', cpf = '${cpfPerfil}' where idFuncionarios = ${idPerfil};
-`;
-
-        console.log("Executando a instrução SQL: \n" + instrucao);
-        return database.executar(instrucao);
+        if (process.env.AMBIENTE_PROCESSO == "producao") {
+            instrucaoSql = `
+            update funcionarios set nome = '${nomePerfil}', email = '${emailPerfil}', senha = '${senhaPerfil}', telefone = '${telefonePerfil}', cpf = '${cpfPerfil}' where idFuncionarios = ${idPerfil};
+        `;
+        } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
+            instrucaoSql = `
+            update funcionarios set nome = '${nomePerfil}', email = '${emailPerfil}', senha = '${senhaPerfil}', telefone = '${telefonePerfil}', cpf = '${cpfPerfil}' where idFuncionarios = ${idPerfil};
+        `;
+        } else {
+            console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
+            return
+        }
+    
+        console.log("Executando a instrução SQL: \n" + instrucaoSql);
+        return database.executar(instrucaoSql);
     }
 
     function UpdateValidadeNova(validadeNova, server) {
-        //console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha);
+        instrucaoSql = ``
 
-        // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
-        //  e na ordem de inserção dos dados.
-        var instrucao = `
-
-
-    
-        UPDATE locacao
+        if (process.env.AMBIENTE_PROCESSO == "producao") {
+            instrucaoSql = `
+            UPDATE locacao
         SET dataValidade = '${validadeNova}'
         WHERE fkServidor = ${server};
-`;
+        `;
+        } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
+            instrucaoSql = `
+            UPDATE locacao
+        SET dataValidade = '${validadeNova}'
+        WHERE fkServidor = ${server};
+        `;
+        } else {
+            console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
+            return
+        }
+    
+        console.log("Executando a instrução SQL: \n" + instrucaoSql);
+        return database.executar(instrucaoSql);
 
-        console.log("Executando a instrução SQL: \n" + instrucao);
-        return database.executar(instrucao);
     }
 
 
 
 
     function atualizarNivelAcesso(NovoNivel, Email) {
-        console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", NovoNivel, Email);
+        instrucaoSql = ``
 
-        // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
-        //  e na ordem de inserção dos dados.
-        var instrucao = `
-    UPDATE funcionarios AS f1
+        if (process.env.AMBIENTE_PROCESSO == "producao") {
+            instrucaoSql = `
+            UPDATE funcionarios AS f1
     JOIN (SELECT idFuncionarios FROM funcionarios WHERE email = '${Email}') AS f2
     ON f1.idFuncionarios = f2.idFuncionarios
     SET f1.fkEscalonamento = ${NovoNivel};
-`;
+        `;
+        } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
+            instrucaoSql = `
+            UPDATE funcionarios AS f1
+    JOIN (SELECT idFuncionarios FROM funcionarios WHERE email = '${Email}') AS f2
+    ON f1.idFuncionarios = f2.idFuncionarios
+    SET f1.fkEscalonamento = ${NovoNivel};
+        `;
+        } else {
+            console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
+            return
+        }
+    
+        console.log("Executando a instrução SQL: \n" + instrucaoSql);
+        return database.executar(instrucaoSql);
 
-        console.log("Executando a instrução SQL: \n" + instrucao);
-        return database.executar(instrucao);
     }
 
 
     function excluirConta(NovoNivel, Email) {
-        console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", NovoNivel, Email);
+        instrucaoSql = ``
 
-        // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
-        //  e na ordem de inserção dos dados.
-        var instrucao = `
-    DELETE FROM funcionarios
-WHERE email = '${Email}' AND fkEscalonamento = ${NovoNivel};
-`;
-
-        console.log("Executando a instrução SQL: \n" + instrucao);
-        return database.executar(instrucao);
+        if (process.env.AMBIENTE_PROCESSO == "producao") {
+            instrucaoSql = `
+            DELETE FROM funcionarios
+            WHERE email = '${Email}' AND fkEscalonamento = ${NovoNivel};
+        `;
+        } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
+            instrucaoSql = `
+            DELETE FROM funcionarios
+            WHERE email = '${Email}' AND fkEscalonamento = ${NovoNivel};
+        `;
+        } else {
+            console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
+            return
+        }
+    
+        console.log("Executando a instrução SQL: \n" + instrucaoSql);
+        return database.executar(instrucaoSql);
     }
 
     module.exports = {
